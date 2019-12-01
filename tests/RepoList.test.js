c541190b32150390a319625d36f1899d3fae64ae
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-11-30 21:25:33 -0800

    Add RepoList tests

--- tests/RepoList.test.js
+++ tests/RepoList.test.js
@@ -3,10 +3,10 @@ import RepoList from '../client/src/components/RepoList';
 import { mount } from 'enzyme';
 
 describe('<RepoList />', () => {
-  it('should have a div element has outermost node', () => {
+  it('should have a div element as the first child containing table elements', () => {
     const wrapper = mount(<RepoList repos={[]}/>);
-    expect(wrapper.exists('div')).toEqual(true);
-    // expect(wrapper.find('div').parent().exists()).toEqual(false);
+    expect(wrapper.children().find('div').parent().is('RepoList')).toEqual(true);
     expect(wrapper.find('div').children().exists()).toEqual(true);
+    expect(wrapper.find('table').parent().is('div')).toEqual(true);
   })
 });
